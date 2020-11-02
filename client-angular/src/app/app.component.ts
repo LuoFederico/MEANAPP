982ef9f0158ef7ccac0419741ce680b5ced67655
LuoFederico <luo.hang.li.federico@iisgalvanimi.edu.it>
2020-11-02 11:54:41 +0000

    MeanApp

---
+++ client-angular/src/app/app.component.ts
@@ -0,0 +1,25 @@
+
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http'; //HTTP CLIENT
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title(title: any) {
+    throw new Error("Method not implemented.");
+  }
+  results: string[];
+  private BASE_URL:string = '/api';
+    // Inject HttpClient into your component or service.
+    constructor(private http: HttpClient) {}
+
+    ngOnInit(): void {
+      // Make the HTTP request:
+      this.http.get('https://3000-f0be7447-5102-4614-9155-7f765075c1a7.ws-eu01.gitpod.io/api').subscribe(data => {
+        // Read the result field from the JSON response.
+        this.results = data['results'];
+      });
+    }
+}
